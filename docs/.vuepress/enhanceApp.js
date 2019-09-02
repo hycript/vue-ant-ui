75f884dd0b53e637cb6f2403e13b6d964ebfd221
LosMessi <kaisuo1010@163.com>
2019-09-02 22:47:40 +0800

    fix for 'Unknown custom element'

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,3 +1,4 @@
+import pageComponents from '@internal/page-components'
 import { Components } from '~/index';
 import ContentSlotsDistributor from '@/pages/common/ContentSlotsDistributor';
 
@@ -7,6 +8,13 @@ export default ({
     router, // 当前应用的路由实例
     siteData // 站点元数据
 }) => {
+    /*
+     * fix for 'Unknown custom element'
+     * https://github.com/vuejs/vuepress/issues/1173
+     */
+    for (const [name, component] of Object.entries(pageComponents)) {
+        Vue.component(name, component);
+    }
     // ...做一些其他的应用级别的优化
     console.log('VUe', Vue, options, router, siteData);
     // Vue.use(UI);
